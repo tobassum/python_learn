d9af1c8c5b09e2e9a20cbdbff27f244753a427fb
Tobassum Munir <tobassum@hotmail.com>
2018-08-22 21:56:59 +0300

    Create while__else.py

---
+++ while__else.py
@@ -0,0 +1,16 @@
+import random
+print("Lucky number 3 number will be generator.")
+print("If those numbers in a one of 5, you will Lose!!")
+
+count = 0
+
+while < 3:
+  num = random.randint(1, 6)
+  print(num)
+  if num == 5:
+    Print("You Lose! SORRY")
+    break
+  count += 1
+else:
+  print("Conratulations!! You Win")
+   
