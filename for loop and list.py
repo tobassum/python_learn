647b0e959954a7040d44295c1be4c04ee47d55d6
Tobassum Munir <tobassum@hotmail.com>
2018-08-24 08:49:58 +0300

    Create for loop and list.py

---
+++ for loop and list.py
@@ -0,0 +1,6 @@
+hobbies = []
+
+for i in range(3):
+    hobby = input("Enter your Hobby. ")    # input() use in python 3
+    hobbies.append(hobby)                  #if you use python 2 so use raw_input() 
+print(hobbies)
