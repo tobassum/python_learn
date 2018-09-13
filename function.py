3c88be290de6aa31a685781d585e16c7e4dbd4e7
Tobassum Munir <tobassum@hotmail.com>
2018-09-13 03:55:51 -0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def Pakistan(self, province, district):
+    self.province = province
+    self.district = district
+    
+def Data(self):
+    print("I live in Pakistan", self.province, "and ", self.district)
+    
+p = input("Enter the Province: ")
+d = input("Enter the District: ")
+print("I live in Pakistan Province is", p, "and District is", d)
+
+# This is my own make code anyone view this code and make correction.
